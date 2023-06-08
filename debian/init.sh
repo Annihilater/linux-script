f8c08d00313fddc299cc4a80c43af8e4fa77bced
klausee <9960100@gmail.com>
2023-06-08 19:19:54 +0800

    feat: update

--- debian/init.sh
+++ debian/init.sh
@@ -8,7 +8,7 @@ NC='\033[0m' # No Color
 ssh_dir="$HOME/.ssh"
 authorized_keys_path="$HOME/.ssh/authorized_keys"
 ssh_public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDRvdkf82cUkGKS1o8yC9hWjxUoJI20rboLpoGpzwSOfw2gHdyLfxdHDZ0+FsMStgIIOkwIAehoSpOjbTyhGwGz9EdyigWMGSm/64XpslHb5GNHDXcHhZCXfGeNjGZwN8srX4uotOvEuW8DX3TE8+MWoJm5LHw+VIdp+RGoEqA1qWDCJO6Afu6RPIFHwuqdOJ0/o2OwaHSBjxG4jWco/XFf229VqEj5VpSeTOpjCnAJfADHBn2KQlMg+pfYeZzBWBm0w5ugstZ+BAhvoQnkM/QKaNJs7+jTPYHHj101Rw90lM6Zq0SBo4WgbOAwBmtR5j5fbvjJPDoeh+aYGLZZ3IX7zpbmNgzOIh0E4Pfw7qA9s2j5Qd2dRLHOtdtD08/4TvibO9wM3i2QJJuwaRoo1opoV3zDIkjuOCLQw50oPjB4MawR3GESgY/wkT/GV3vsnNvUFm/TbSs7FIF14mdEl6MEWjnD7bzNcJ6QRFZMOPOx5eqb13Ph4k3S76imy+Za1LU= vampire"
-sshd_config_path="/etc/ssh/sshd_config.d/ziji.conf"
+sshd_config_path="/etc/ssh/sshd_config.d/default.conf"
 bash_sshd_config_path="/etc/ssh/sshd_config"
 
 # update 
