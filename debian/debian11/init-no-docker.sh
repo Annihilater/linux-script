d1bc3d4fed7a0b4a6a966d8578d0fbe7e8fb6dcb
klausee <9960100@gmail.com>
2023-07-02 16:27:46 +0800

    feat: update

--- debian/debian11/init-no-docker.sh
+++ debian/debian11/init-no-docker.sh
@@ -35,6 +35,8 @@ apt install curl -y
 apt install htop -y
 apt install sudo -y
 apt install zip -y
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+apt install speedtest -y
 
 mkdir /opt/apps
 
